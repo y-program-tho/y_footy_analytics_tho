a0891a04927e49c6272294849977ec9de16b860a
y-program-tho <yanga12edu@gmail.com>
2025-10-30 23:50:18 +0000

    Add files needed for streamlit app

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+import streamlit as st
 import fbref_batch_scraper as fds
 
-epl_data = fds.epl_for_league_data([2025, 2026])
 
-epl_data[4].head(10)
---
+++ pages/2_player.py
@@ -0,0 +1,2 @@
+import streamlit as st
+import fbref_batch_scraper as fds
